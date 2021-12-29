1445f8f18681d50aacafaec147f3b2067de64ad4
Lesnicho <lesnik.artemij@bk.ru>
2021-12-29 16:34:26 +0300

    Сделали Конкатинацию и добавили несколько тегов

--- index.js
+++ index.js
@@ -7,12 +7,27 @@
 //          console.log(result)
 //      })
 
+let p = document.querySelector('.p')
+let img = document.querySelector('.img')
+
 async function getWeather(){
     const info = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=6e2995a96533b594ae4aee19b7723f8d')
     const result = await info.json()
+    
     console.log(result)
     console.log(result.main.temp)
+    
     const temp = result.main.temp - 273
+
     console.log(temp.toFixed(1))
+    //p.innerHTML = 'Погода на улице ' + temp.toFixed(1) 
+    
+    p.innerHTML = `погода на улице ${temp.toFixed(1)}`
+    
+    const icon = result.weather[0].icon
+    console.log(icon) 
+    //img.src = 'http://openweathermap.org/img/wn/'+icon+'@2x.png'
+    
+    img.src = `http://openweathermap.org/img/wn/${icon}@2x.png`
 }
 getWeather();
