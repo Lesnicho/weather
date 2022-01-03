8be18e85128958699cc4202f50ff2a11cbf91078
Lesnicho <lesnik.artemij@bk.ru>
2022-01-03 17:03:17 +0300

    Добавили input для более удобного поиска погоды, добавили стили в css, сделали более приятный вывод результа

--- index.js
+++ index.js
@@ -10,8 +10,13 @@
 let p = document.querySelector('.p')
 let img = document.querySelector('.img')
 
+let input = document.querySelector('.in1')
+let button = document.querySelector('.btn')
+
+
 async function getWeather(){
-    const info = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=6e2995a96533b594ae4aee19b7723f8d')
+    
+    const info = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=6e2995a96533b594ae4aee19b7723f8d`)
     const result = await info.json()
     
     console.log(result)
@@ -20,14 +25,17 @@ async function getWeather(){
     const temp = result.main.temp - 273
 
     console.log(temp.toFixed(1))
-    //p.innerHTML = 'Погода на улице ' + temp.toFixed(1) 
+  //p.innerHTML = 'Погода на улице ' + temp.toFixed(1) 
     
-    p.innerHTML = `погода на улице ${temp.toFixed(1)}`
+    p.innerHTML = `Погода в ${input.value} : ${temp.toFixed(1)}`;
     
+  //p.innerHTML = `Now the weather is outside ${temp.toFixed(1)}`
+
     const icon = result.weather[0].icon
     console.log(icon) 
-    //img.src = 'http://openweathermap.org/img/wn/'+icon+'@2x.png'
+  
+  //img.src = 'http://openweathermap.org/img/wn/'+icon+'@2x.png'
     
     img.src = `http://openweathermap.org/img/wn/${icon}@2x.png`
 }
-getWeather();
+button.onclick = () => getWeather();
